16166394813da902cfabf2f5d16b53ec2abdf558
alexandrecai <alexandre.cailloux@gmail.com>
2025-02-24 15:57:16 +0100

    Adding working navbar

--- Front/personal-website/src/app/app.routes.ts
+++ Front/personal-website/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {MainPageComponent} from './main-page/main-page.component';
+import {ContactPageComponent} from './contact-page/contact-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path : 'contact', component : ContactPageComponent},
+  { path : '', component : MainPageComponent}
+];
