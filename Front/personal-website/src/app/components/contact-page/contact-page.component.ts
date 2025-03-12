c09ac2749f9ae0f6deccad22e540548ae9e08666
alexandrecai <alexandre.cailloux@gmail.com>
2025-03-12 14:31:48 +0100

    Creating contact page and allowing the website to send email with EmailJS

--- Front/personal-website/src/app/components/contact-page/contact-page.component.ts
+++ Front/personal-website/src/app/components/contact-page/contact-page.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import {FormsModule, NgForm, ReactiveFormsModule} from '@angular/forms';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormField,
+    ReactiveFormsModule,
+    MatInput,
+    MatLabel,
+  ],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
 export class ContactPageComponent {
 
+  onSubmitForm(contactForm: NgForm) {
+    emailjs.send("service_45hjtq4", "template_opqi7fw", {...contactForm.value}, {
+      publicKey: 'aJbao29nAOj2Dg4E1'
+    }).then(()  => {
+        console.log("Email sent")
+      },
+      (error) => {
+        console.log('FAILED...', (error as EmailJSResponseStatus).text);
+      },  )
+  }
 }
