ada4e755fb6f8516ae3dcf3f82dc734235acaa3d
alexandrecai <alexandre.cailloux@gmail.com>
2025-04-07 23:56:07 +0200

    Improving project item cards

--- Front/personal-website/src/app/components/projet-item/projet-item.component.ts
+++ Front/personal-website/src/app/components/projet-item/projet-item.component.ts
@@ -1,8 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Projet} from '../../models/projet';
-import {NgIf, NgOptimizedImage} from '@angular/common';
-import {MatChip, MatChipListbox, MatChipSet} from '@angular/material/chips';
-import {MatExpansionPanel, MatExpansionPanelTitle, MatExpansionPanelHeader} from '@angular/material/expansion';
+import {NgIf, NgOptimizedImage, UpperCasePipe} from '@angular/common';
+import {MatChip, MatChipSet} from '@angular/material/chips';
 import {MatIcon} from '@angular/material/icon';
 
 @Component({
@@ -12,10 +11,8 @@ import {MatIcon} from '@angular/material/icon';
     MatChipSet,
     MatChip,
     NgIf,
-    MatExpansionPanel,
-    MatExpansionPanelTitle,
-    MatExpansionPanelHeader,
-    MatIcon
+    MatIcon,
+    UpperCasePipe
   ],
   templateUrl: './projet-item.component.html',
   styleUrl: './projet-item.component.scss'
--- Front/personal-website/src/app/services/projet.service.ts
+++ Front/personal-website/src/app/services/projet.service.ts
@@ -13,7 +13,7 @@ export class ProjetService {
         new Projet(
           "Site web personnel",
           "Création de mon site web personnel servant de vitrine pour mes réalisations. Ce site, sur lequel vous naviguez actuellement, a été entièrement développé avec Angular. Il intègre la bibliothèque EmailJS, permettant de me contacter facilement par email.",
-          "/logo.jpeg",
+          "/logo_nobackground.png",
           ["Alexandre Cailloux"],
           ["Angular","Bootstrap"],
           "https://github.com/alexandrecai/PersonalWebsite",
@@ -23,7 +23,15 @@ export class ProjetService {
           "Site web pour une pizzeria",
           "DiLL est un défi qui met en relation des étudiants de l’Université d’Orléans, de l’Université de Tours et de l’INSA Centre-Val de Loire avec des entreprises, des associations, des collectivités et des institutions de la région Centre-Val de Loire. Chaque organisation propose un projet sur lequel les étudiants travaillent en groupe.\n" +
           "\n" +
-          "Mon groupe a été chargé de créer un site web et une application mobile pour une pizzeria locale. Le site permet aux utilisateurs de passer des commandes en ligne, tandis que l’application mobile est utilisée par le personnel de cuisine pour afficher les commandes." ,
+          "Mon groupe a été chargé de créer un site web et une application mobile pour une pizzeria locale. Le site permet aux utilisateurs de passer des commandes en ligne, tandis que l’application mobile est utilisée par le personnel de cuisine pour afficher les commandes.\n" +
+          "\n" +
+          " - Développement du backend avec Spring et MongoDB\n" +
+          "\n" +
+          " - Création du frontend web avec Angular\n" +
+          "\n" +
+          " - Mise en place d’un environnement Docker\n" +
+          "\n" +
+          " - Communication avec le client pour répondre à ses besoins",
           "/petitesaisonlogo.jpg",
           ["Alexandre Cailloux","Arnaud Girard","Matthieu Bonbon","Fatoumata Barry"],
           ["Java","Spring","Angular","MongoDB"],
